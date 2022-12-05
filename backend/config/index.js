c32f254f58f3f441a6b817d02ba838e32c225a73
KapilKharera <kapil95881@gmail.com>
2022-12-05 16:41:01 +0530

    Add environment variables

--- backend/config/index.js
+++ backend/config/index.js
@@ -5,6 +5,8 @@ dotenv.config();
 const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+    MONGO_URL: process.env.MONGO_URL,
+    PORT: process.env.PORT
 };
 
 export default config;
