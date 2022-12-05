99d80af40671b9e06c3fd64831b8878f2b1bd5a2
KapilKharera <kapil95881@gmail.com>
2022-12-06 01:45:11 +0530

    Add utility for email

---
+++ backend/utils/mailHelper.js
@@ -0,0 +1,15 @@
+import transporter from '../config/transporter.config';
+import config from '../config/index';
+
+const mailHelper = async (options) => {
+    const message = {
+        from: config.SMTP_MAIL_SENDERMAIL,
+        to: options.email,
+        subject: options.subject,
+        text: options.text
+    }
+
+    await transporter.sendMail(message);
+}
+
+export default mailHelper;
