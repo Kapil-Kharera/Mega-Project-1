e3f814034c7eb78e74508d9cb1a021e2c301bf0a
KapilKharera <kapil95881@gmail.com>
2022-12-06 01:43:54 +0530

    Add environment varialbes for mail

--- backend/config/index.js
+++ backend/config/index.js
@@ -6,7 +6,12 @@ const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
     MONGO_URL: process.env.MONGO_URL,
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    SMTP_MAIL_HOST: process.env.SMTP_MAIL_HOST,
+    SMTP_MAIL_PORT: process.env.SMTP_MAIL_PORT,
+    SMTP_MAIL_USERNAME: process.env.SMTP_MAIL_USERNAME,
+    SMTP_MAIL_PASSWORD: process.env.SMTP_MAIL_PASSWORD,
+    SMTP_MAIL_SENDERMAIL: process.env.SMTP_MAIL_SENDERMAIL,
 };
 
 export default config;
