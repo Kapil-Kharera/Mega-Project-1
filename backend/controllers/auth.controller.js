53c6ecd94d29f71bea0a0a65939b723adfe08f7c
KapilKharera <kapil95881@gmail.com>
2022-12-06 14:42:03 +0530

    Add authController - getProfile

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -222,3 +222,26 @@ export const resetPassword = asyncHandler(async (req, res) => {
 
 //TODO
 // create a controller for change password
+
+/********************** 
+*@RESET_PASSWORD
+*@REQUEST_TYPE GET
+*@route http://localhost:4000/api/auth/profile
+*@description check for token and populate req.user
+*@parameters 
+*@return User Object
+
+**************************/
+
+export const getProfile = asyncHandler(async(req, res) => {
+    const { user } = req
+
+    if (!user) {
+        throw new CustomError("User not found", 404);
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+});
