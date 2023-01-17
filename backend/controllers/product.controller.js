71b05007dad600f9161e1e447c23abaab8e35cc9
KapilKharera <kapil95881@gmail.com>
2023-01-17 19:16:48 +0530

    Added more features to productConroller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -15,6 +15,7 @@ import config from '../config';
  *@description Controller used for creating a new product
  *@description Only admin can create the coupon
  *@description Uses AWS S3 Bucket for image upload
+ *@return Products Object
 
  ***************************************************/
 
@@ -89,4 +90,53 @@ import config from '../config';
            });
         }
     });
+ });
+
+
+ /***************************************************
+ *@GET_ALL_PRODUCT
+ *@route https://localhost:5000/api/product
+ *@description Controller used for getting all products details
+ *@description User and admin can get all the products
+ *@return Products Object
+
+ ***************************************************/
+
+ export const getAllProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    if (!products) {
+        throw new CustomError("NO product was not found", 404);
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "All Products are :",
+        products
+    });
+ });
+
+/***************************************************
+ *@GET_PRODUCT_BY_ID
+ *@route https://localhost:5000/api/product
+ *@description Controller used for getting single product details
+ *@description User and admin can get single product details
+ *@return Products Object
+
+ ***************************************************/
+
+ export const getProductById = asyncHandler(async (req, res) => {
+    const {id: productId} = req.params;
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+        throw new CustomError("No such product is exist", 404);
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Your product is : ",
+        product
+    })
  });
